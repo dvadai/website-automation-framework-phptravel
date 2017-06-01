438608bf9feed072b782c2071d95b2ac996706e1
Diana Vadai <diana.vadai@gmail.com>
2017-06-01 21:38:03 +0100

    Extend with login tests

--- src/test/java/phptravels/pagefiles/LandingPage.java
+++ src/test/java/phptravels/pagefiles/LandingPage.java
@@ -11,7 +11,8 @@ public class LandingPage extends BasePage {
     private static final String MY_ACCOUNT_BUTTON_TEXT = "My Account";
     private static final String MY_ACCOUNT_BUTTON_LOCATOR = "i.icon_set_2_icon-105";
     private static final String URL = "http://www.phptravels.net/";
-
+    private static final String LOGIN_BUTTON_LOCATOR = ".dropdown-menu > li > a";
+    private static final String LOGIN_BUTTON_TEXT = "Login";
 
 
     public LandingPage(WebDriver webDriver){
@@ -25,5 +26,19 @@ public class LandingPage extends BasePage {
     public boolean isDisplayed(){
         WebElement myAccountButton = driver.findElement(By.cssSelector(MY_ACCOUNT_BUTTON_LOCATOR));
         return myAccountButton.getText().contains(MY_ACCOUNT_BUTTON_TEXT);
+    }
+
+    public void clickOnAccount(){
+        WebElement myAccountButton = driver.findElement(By.cssSelector(MY_ACCOUNT_BUTTON_LOCATOR));
+        myAccountButton.click();
+
+    }
+
+
+    public void clickOnLogin(){
+        WebElement loginButton = driver.findElement(By.cssSelector(LOGIN_BUTTON_LOCATOR));
+        loginButton.click();
+
+
     }
 }
